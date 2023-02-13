72007206638211aa72e816c6a9c552d6bebf21d7
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-02-12 21:08:01 -0500

    Se vinculan los videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -22,7 +22,7 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/W7-HlELqAg0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/gwfhsCGexYc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -35,7 +35,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/W7-HlELqAg0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     .separador.p3.mb-5
 
